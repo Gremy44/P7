da2f6d66ccf576ed730768b920ab780c17c766b9
Remy Gosselin <gosselin.remy@gmail.com>
2022-11-15 14:34:27 +0400

    Ajout du rapport flake8

--- brutforce.py
+++ brutforce.py
@@ -6,12 +6,11 @@ class BruteForce:
     def __init__(self, pathfile):
         self.pathfile = pathfile
 
-    
     def put_in_lst(self):
         '''
         - extract value from csv and push it in dict
         - take 'filePathToCSV'
-        - return [('action_name1',price, profit),('action_name2','value1', 'value2'),...]
+        - return [('action_name1',price, profit),('action_name2','value1','value2'),...]
         '''
         values_container = []
 
@@ -20,53 +19,57 @@ class BruteForce:
             reader = csv.reader(csvfile, delimiter=',', quotechar='|')
             for row in reader:
                 # create dict with {name:(price,profit)}
-                values_container.append((row[0],float(row[1]), float(row[1])*float(row[2])/100))
+                values_container.append(
+                    (row[0], float(row[1]), float(row[1])*float(row[2])/100))
 
         return values_container
 
     def brut_force(self, wallet, elements, action_selection=[]):
-        
+
         if elements:
-            
+
             # del first value of 'elements'list and return it in val1
             val1 = elements.pop(0)
-            
-            # rest1 and rest2 solution actuelle et la precedente
-            rest1 = self.brut_force(wallet, elements.copy(), action_selection+[val1])
+
+            # rest1 and rest2 double recursion
+            rest1 = self.brut_force(wallet, elements.copy(), action_selection + [val1])
 
             rest2 = self.brut_force(wallet, elements.copy(), action_selection)
 
-            # move aside following situation
-            if rest2 == None and rest1 != None:
+            # avoid going to the comparison if at least one none
+            if rest2 is None and rest1 is not None:
                 return rest1
 
-            if rest1 == None and rest2 != None:
+            if rest1 is None and rest2 is not None:
                 return rest2
 
-            if rest1 == None and rest2 == None:
+            if rest1 is None and rest2 is not None:
                 return None
 
             # keep the best solution
-            if rest1[1]>rest2[1]:
+            if rest1[1] > rest2[1]:
                 return rest1
-            else :
+            else:
                 return rest2
 
         else:
             # total and benefice calculation for return
             total = sum([action_selection[i][1] for i in range(len(action_selection))])
-            benefices = sum([action_selection[i][2] for i in range(len(action_selection))])
+            benefices = sum([action_selection[i][2] for i in range(
+                len(action_selection))])
 
             if total > wallet:
                 return None
             else:
                 return [total, benefices, action_selection, wallet]
 
+
 start_time = time.time()
 
-#path = 'data\1-Brut_force\data_brut_force.csv'
+# path = 'data\1-Brut_force\data_brut_force_test.csv'
 path = sys.argv[1]
 mes_valeur = BruteForce(path)
+print(mes_valeur.put_in_lst())
 retour_valeur = mes_valeur.brut_force(500, mes_valeur.put_in_lst())
 
 # retours console
--- optimized.py
+++ optimized.py
@@ -23,19 +23,21 @@ class Optimized:
                     # excluse wrong values
                     if float(row[1]) > 0.0:
                         if float(row[2]) > 0.0:
-                        # create clean list of values
-                            values_container.append((row[0], int(float(row[1])*100), int(round(float(row[1])*float(row[2])/100, 2)*100)))
+                            # create clean list of values
+                            values_container.append(
+                                (row[0], int(float(row[1])*100),
+                                    int(round(float(row[1])*float(row[2])/100, 2)*100)))
                 except ValueError:
                     pass
-        
+
         return values_container
 
     def optimized(self, wallet, elements):
 
         # convert float value to int
         wallet = wallet*100
-        
-        # make array 
+
+        # make array
         matrice = [[0 for x in range(wallet + 1)] for x in range(len(elements) + 1)]
 
         # browse elements
@@ -44,13 +46,15 @@ class Optimized:
 
                 # keep the max value if lower of wallet
                 if elements[i-1][1] <= w:
-                    matrice[i][w] = max(elements[i-1][2] + matrice[i-1][w-elements[i-1][1]], matrice[i-1][w])
+                    matrice[i][w] = max(
+                        elements[i-1][2] + matrice[i-1][w-elements[i-1][1]],
+                        matrice[i-1][w])
                 else:
                     # keep result of previous line if highter
                     matrice[i][w] = matrice[i-1][w]
-        
+
         # return element by sum of them
-        # we browse reverse the matrice for find the keeped elements 
+        # we browse reverse the matrice for find the keeped elements
 
         w = wallet
         n = len(elements)
@@ -70,26 +74,29 @@ class Optimized:
 
         ttcost = 0
         action_format = []
-        
+
         for i in action_selection:
             # generate cost value
             ttcost += i[1]
 
             # format action selection value
             action_format.append((i[0], i[1]/100, i[2]/100))
-        
+
         # Format total cost value
         ttcost = ttcost/100
-        
-        #return print("Profit : ", (matrice[-1][-1])/100), print("Total cost : ", ttcost), print("Actions selection : ", action_format)
+
+        # return print("Profit : ", (matrice[-1][-1])/100),
+        # print("Total cost : ", ttcost),
+        # print("Actions selection : ", action_format)
         return [ttcost, (matrice[-1][-1])/100, action_format, wallet/100]
+
 
 start_time = time.time()
 
-#path = './data/2-Optimise/dataset2_Python+P7.csv'
+# path = './data/2-Optimise/dataset2_Python+P7.csv'
 path = sys.argv[1]
 mes_valeur = Optimized(path)
-retour_valeur = mes_valeur.optimized(500, mes_valeur.put_in_lst()) 
+retour_valeur = mes_valeur.optimized(500, mes_valeur.put_in_lst())
 
 # retours console
 print("-------------------------------------------------------------")
